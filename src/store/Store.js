6c68a258999e1bdaf8e5b9c6ba6da43bff891887
rpostkg <190353914+rpostkg@users.noreply.github.com>
2026-02-17 18:19:06 +0200

    Store class

---
+++ src/store/Store.js
@@ -0,0 +1,32 @@
+class Store {
+    constructor() {
+        this.orders = [];
+        this.couriers = [];
+    }
+
+    addOrder(order) {
+        this.orders.push(order);
+    }
+
+    addCourier(courier) {
+        this.couriers.push(courier);
+    }
+
+    getPendingOrders() {
+        return this.orders.filter(o => o.status === 'PENDING');
+    }
+
+    getIdleCouriers() {
+        return this.couriers.filter(c => c.status === 'IDLE');
+    }
+
+    getOrderById(id) {
+        return this.orders.find(o => o.id === id);
+    }
+
+    getCourierById(id) {
+        return this.couriers.find(c => c.id === id);
+    }
+}
+
+export const store = new Store();
