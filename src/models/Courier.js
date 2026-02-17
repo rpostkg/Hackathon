3cca0150f7af32463ccd5e65a84753387b648724
rpostkg <190353914+rpostkg@users.noreply.github.com>
2026-02-17 19:19:34 +0200

    Means of transportation for couriers

--- src/models/Courier.js
+++ src/models/Courier.js
@@ -1,12 +1,29 @@
+export const TransportType = {
+    WALKER: 'Walker',
+    BICYCLE: 'Bicycle',
+    CAR: 'Car',
+};
+
+export const TransportCapacity = {
+    [TransportType.WALKER]: 5,
+    [TransportType.BICYCLE]: 15,
+    [TransportType.CAR]: 50,
+};
+
 export const CourierStatus = {
     FREE: 'Free',
     BUSY: 'Busy',
 };
 
 export class Courier {
-    constructor(id, location) {
+    constructor(id, location, transportType = TransportType.WALKER) {
         this.id = id;
         this.location = location; // { x, y }
+        this.transportType = transportType;
         this.status = CourierStatus.FREE;
+    }
+
+    get capacity() {
+        return TransportCapacity[this.transportType] || 0;
     }
 }
